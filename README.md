9877ef2517e94ac25eb794e97dc412289a386adf
Jason Snyder <jason.snyder159@gmail.com>
2022-02-27 16:28:01 -0500

    labeling JS, HTML, working on README.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Day-Planner
+
+## [Day Planner](https://jsnyder159.github.io/Day-Planner/)
+![Day Planner Top](./assets/images/planner-top.PNG)
+![Day Planner Bottom](./assets/images/planner-botton.PNG)
+
+## Description
+
+Task is to make a calender application that allows a user to save events for each hour of normal business hours.
+
+Criteria:  The daily planner when opened should display the current day at the top of the calender.  When scrolling through the application there should be time blocks for standard business hours.  When viewing the time blocks they should have a color associated with them for past, present, and future times.  When clicking on the time block you should be able to input an event.  When the save button is clicked the input information should be saved to local storage.  When the page is refreshed the saved events should persist.
+
+Steps Taken:
+
+- HTML built so that all hours are there. (0800-1700hrs.)
+
+- Bootstrap used for proper display layout.
+
+- CSS used for style format.
+
+- Save button added and icon given to it.
+
+- jQuery used to make functions.
+
+- Moment used to give current day information.
+
+- Local storage used to save information put into display boxes.
+
+- getItem used to pull information saved in local storage so information persists through page refresh.
+
+- Function created to check the current time VS the time blocks associated time.  If its a past time color will be grey, if current time it will be red, and if future time color for block will be green.
+
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+
+
 let today = moment().format('dddd, MMM Do YYYY');
 
+// Gives current Day, Month, Date, Year to main header.  Above is how the current moment is defined
 $("#currentDay").text(today);
 
+
+// Function for save button
 $(document).ready(function () {
 
+    // on click saves the text with associated time to local storage
     $(".saveBtn").on("click", function() {
 
     let text = $(this).siblings(".description").val();
@@ -12,21 +18,27 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
 })
 
+// Function for colorization of day planner.
 function timeTracker() { 
     let timeNow = moment().hour();
     $(".time-block").each(function () {
         let blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+        // If time has pasted from the current time (timeNow) will color the box as grey.
         if(blockTime < timeNow) {
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
         }
+
+        // If time block is the same time as current time (timeNow) will color the box red.
         else if (blockTime === timeNow) {
             $(this).removeClass("future");
             $(this).addClass("present");
             $(this).removeClass("past");
         }
+
+        // If time block is a future time from the current time (timeNow) will color the box green.
         else {
             $(this).addClass("future");
             $(this).removeClass("present");
@@ -35,6 +47,7 @@ function timeTracker() {
     })
 }
 
+// Displays the input given to the description area that was saved to local storage. 
 $("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -43,6 +56,8 @@ $("#hour12 .description").val(localStorage.getItem("hour12"));
 $("#hour13 .description").val(localStorage.getItem("hour13"));
 $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 timeTracker();
 })
